bb22e5b9ac61261e9515929d83d965fc0743b0d3
Bruffen <bruff.dev@gmail.com>
2024-09-23 14:03:37 +0100

    More complete CMake file

--- compile.bat
+++
@@ -1,3 +0,0 @@
-C:\VulkanSDK\1.3.290.0\Bin\glslc.exe .\shaders\simple.vert -o .\shaders\simple.vert.spv
-C:\VulkanSDK\1.3.290.0\Bin\glslc.exe .\shaders\simple.frag -o .\shaders\simple.frag.spv
-pause
